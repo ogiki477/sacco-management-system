7a0ce4423ee50a25d82f45c1dc296b2281d8782d
ogiki477 <omojick88moses@gmail.com>
2024-10-20 13:20:48 +0300

    created a LoanUserController

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoanController;
 use App\Http\Controllers\LoanPlanController;
 use App\Http\Controllers\LoanTypesController;
+use App\Http\Controllers\LoanUserController;
 use App\Http\Controllers\StaffController;
 use Illuminate\Support\Facades\Route;
 
@@ -48,8 +49,14 @@ Route::group(['middleware' => 'Admin'],function(){
     Route::post('admin/loan_plan/edit/{id}',[LoanPlanController::class,'insert_edit_loan_plan_add']);
     Route::get('admin/loan_plan/delete/{id}',[LoanPlanController::class,'delete_loan_plan_add']); 
 
+    //loan menu
     Route::get('admin/loans/list',[LoanController::class,'index']);    
     Route::get('admin/loans/add',[LoanController::class,'create']);    
+
+
+    //Loan User 
+    Route::get('admin/loan_user/list',[LoanUserController::class,'index']);
+
 
     
 
